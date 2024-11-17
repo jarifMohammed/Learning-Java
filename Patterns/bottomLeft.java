0ed938cfdf1b439a42cf1d99b54eadd55872d715
Jarif <mohammedjarif@protonmail.com>
2024-11-17 14:42:10 +0600

    Patterns

---
+++ Patterns/bottomLeft.java
@@ -0,0 +1,20 @@
+package Patterns;
+
+public class bottomLeft {
+
+    
+public static void main(String[] args) {
+    int n =5;
+    for(int row = 1 ; row<=n ; row++){
+        for(int col=1 ; col<=n ;col++){
+            if(row - col>=0){
+                System.out.print("*");
+            }
+            
+        }
+        System.out.println();
+    }
+}
+
+    
+}
