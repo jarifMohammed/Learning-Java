8f776ffdec0aabdef69d24db6d46c367c82b1d88
Jarif <mohammedjarif@protonmail.com>
2024-12-02 22:59:41 +0600

    Bit manuplation

--- BitManu/Getithbit.java
+++ BitManu/Getithbit.java
@@ -39,11 +39,28 @@ public class Getithbit {
         int bitMask = (~0)<<i;
         return n & bitMask;
     }
+    // clear bits in specific range 
+    public static int clearBitsinrange(int n , int i , int j){
+        int a = (~0)<<(j+1);
+        int b = (1<<i) -1;
+        int bitMask =a|b;
+        return n & bitMask;
+    }
+
+    // find if it is power of 2
+    public static boolean isPowerOfTwo(int n){
+        
+        
+        
+        return (n&(n-1))==0;
+    }
     public static void main(String[] args) {
         System.out.println(getBit(10, 3));
         System.out.println(setBit(10, 2));
         System.out.println(clearBit(10, 1));
         System.out.println(updateBit(10,2 , 1));
         System.out.println(clearIBits(15, 2));
+        System.out.println(clearBitsinrange(10, 2, 4));
+        System.out.println(isPowerOfTwo(8));
     }
 }
